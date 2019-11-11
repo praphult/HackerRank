e2f7d2d6c9e4764ff85427fe1b21109decea743d
Praphul <praphul@sarayu>
2019-11-11 14:20:34 +0530

    First commit

---
+++ C/HelloWorld.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+
+int main() 
+{
+	char input_string[105] ;
+	scanf("%[^\n]", input_string) ;
+	
+	printf("Hello world\n \n") ;
+	
+	printf("%s",input_string) ;
+	
+	return 0 ;
+}
+ 
---
+++ Cpp/HelloWorld.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+	string input_string ;
+	getline(cin, input_string) ;
+	
+	cout << "Hello world " << endl ;
+	
+	cout << input_string << endl ;
+	
+	return 0 ;
+	
+}
